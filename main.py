516d788830d2c08c215265950a8da4c975de0d5e
maxim-komlik <maxim.komlik@tut.by>
2021-10-10 17:51:49 +0300

    Histograms

--- main.py
+++ main.py
@@ -34,11 +34,7 @@ class MinMaxFilter:
         if type(img) is not np.ndarray:
             img = np.array(img)
         _hb = int(pool[0]/2)
-        _vb = int(pool[0]/2)
-        """result = np.zeros(shape=(img.shape[0]-2*_hb, img.shape[1]-2*_vb, *img.shape[2:]), dtype=np.uint8)
-        for x in range(result.shape[0]):
-            for y in range(result.shape[1]):
-                result[x, y] = np.amin(img[x:x + pool[0], y:y + pool[1]], axis=(0, 1))"""
+        _vb = int(pool[1]/2)
 
         result = np.zeros(shape=(img.shape[0]+2*_hb, img.shape[1]+2*_vb, *img.shape[2:]), dtype=np.uint8)
         result[_hb:-_hb, _vb:-_vb] = img
@@ -46,10 +42,6 @@ class MinMaxFilter:
         np.transpose(result, axes=(1, 0, 2))
         result[_vb:-_vb] = MinMaxFilter._apply_pooled(result, np.minimum, np.iinfo(result.dtype).max, pool[1])
         np.transpose(result, axes=(1, 0, 2))
-        """result = np.zeros(shape=img.shape)
-        for x in range(1, img.shape[0]-1):
-            for y in range(1, img.shape[1]-1):
-                result[x, y] = np.amin(img[x - _hb:x + _hb + 1, y - _vb:y + _vb + 1], axis=(0, 1))"""
         return result[_hb:-_hb, _vb:-_vb]
 
     @staticmethod
@@ -57,7 +49,7 @@ class MinMaxFilter:
         if type(img) is not np.ndarray:
             img = np.array(img)
         _hb = int(pool[0]/2)
-        _vb = int(pool[0]/2)
+        _vb = int(pool[1]/2)
 
         result = np.zeros(shape=(img.shape[0]+2*_hb, img.shape[1]+2*_vb, *img.shape[2:]), dtype=np.uint8)
         result[_hb:-_hb, _vb:-_vb] = img
@@ -75,18 +67,26 @@ class MinMaxFilter:
 
 class ImageAnalyser:
     @staticmethod
-    def hist(x, n_bins=20):
+    def hist(x, n_bins=20, dynamic_range=True, low=0, high=255):
         n_bins = int(n_bins)
         if type(x) != np.ndarray:
             x = np.array(x)
-        x_min = np.amin(x)
-        x_range = np.amax(x) - x_min
+        if dynamic_range:
+            x_min = np.amin(x)
+            x_range = np.amax(x) - x_min
+        else:
+            x_min = low
+            x_range = high - x_min
         bin_step = float(x_range) / n_bins
         bins = [bin_step * i for i in range(n_bins + 1)]
         if bin_step > 0:
             result = [0] * n_bins
-            for item in x:
-                result[int((item - x_min) / bin_step)-1] += 1
+            x = (x - x_min) / bin_step - 1
+            i_x = x.astype(int)
+            for item in i_x.flat:
+                result[item] += 1
+            """for item in x.flat:
+                result[int((item - x_min) / bin_step)-1] += 1"""
         else:
             raise ZeroDivisionError
         return bins, result
@@ -116,22 +116,58 @@ def lab(path, bin_num=16):
     img = plt.imread(path)
     dissected = ImageDissector.dissect(img, (params[0], params[1]), (params[2], params[3]))
     filtered_min = MinMaxFilter.min_filter(img, (pool_params[0], pool_params[1]))
-    # filtered_max = MinMaxFilter.max_filter(img)
+    filtered_max = MinMaxFilter.max_filter(img, (pool_params[0], pool_params[1]))
     filtered_minmax = MinMaxFilter.minmax_filter(img, (pool_params[0], pool_params[1]))
-    # bins, hist = ImageAnalyser.hist(img, bin_num)
-    # plt.figure().add_subplot()
-    # plt.gca().hist(bins[:-1], bins, weights=hist)
 
-    figures = [plt.figure()]
-    figures[0].add_subplot().imshow(img)
-    figures.append(plt.figure())
-    figures[1].add_subplot().imshow(dissected)
-    figures.append(plt.figure())
-    figures[2].add_subplot().imshow(filtered_min)
-    figures.append(plt.figure())
-    figures[3].add_subplot().imshow(filtered_minmax)
+    plot_content = [
+        [img],
+        [dissected],
+        [
+            [filtered_min],
+            [filtered_max]
+        ],
+        [filtered_minmax]
+    ]
+    figures = []
+
+    def build_histograms(collection: list):
+        for _item in collection:
+            if type(_item) is tuple:
+                continue
+            if type(_item) is list:
+                build_histograms(_item)
+            else:
+                if len(_item.shape) == 3:
+                    for _i in range(_item.shape[2]):
+                        collection.append(ImageAnalyser.hist(_item[..., _i], bin_num, False))
+                else:
+                    collection.append(ImageAnalyser.hist(_item, bin_num, False))
+
+    for item in plot_content:
+        if type(item) is list:
+            build_histograms(item)
+
+    for item in plot_content:
+        fig = plt.figure()
+        if type(item) is list and len(item) > 0:
+            if type(item[0]) is list:
+                axes = fig.subplots(len(item), len(item[0]))
+                for i in range(len(item)):
+                    if type(item[i]) is list:
+                        axes[i][0].imshow(item[i][0])
+                        axes[i][0].set_axis_off()
+                        for j in range(1, len(item[i])):
+                            if type(item[i][j]) is tuple:
+                                axes[i][j].hist(item[i][j][0][:-1], item[i][j][0], weights=item[i][j][1])
+            else:
+                axes = fig.subplots(1, len(item))
+                axes[0].imshow(item[0])
+                axes[0].set_axis_off()
+                for j in range(1, len(item)):
+                    if type(item[j]) is tuple:
+                        axes[j].hist(item[j][0][:-1], item[j][0], weights=item[j][1])
+        figures.append(fig)
     plt.show()
-    pass
 
 
 if __name__ == '__main__':
